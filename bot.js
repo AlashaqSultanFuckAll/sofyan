27d776702c3f5c012e3124346a3cd3ad54363551
AlashaqSultanFuckAll <40842773+AlashaqSultanFuckAll@users.noreply.github.com>
2018-10-29 14:29:24 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,64 +1,71 @@
+var ServerID = "494284550789136384"; //اي دي السيرفر
+var ChannelID = "506444394463887380";// اي دي الروم
+const prefix = '~'
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const prefix = '4'
-client.on('ready', () => {
-  console.log('======================================')
-  console.log(`Logged in as ${client.user.tag}!`);
-  console.log('')
-  console.log(`servers! [ " ${client.guilds.size} " ]`);
-  console.log('')
-  console.log(`Users! [ " ${client.users.size} " ]`);
-  console.log('=======================================')
-});
 
 
 client.on('message', message => {
-  if (message.author.bot) return;
-  if (!message.content.startsWith(prefix)) return;
-
-  let command = message.content.split(" ")[0];
-  command = command.slice(prefix.length);
-
-  let args = message.content.split(" ").slice(1);
-
-
-
-if (command == "say") {
-let rank = message.guild.member(message.author).roles.find('name', '1');
-if (!rank) return message.reply('انت لا تمتلك الرتبه المخصصه لهذا الامر')
-  message.channel.send(args.join("  "))
-    message.delete();
-  }
-});
-
-
-
-client.on('message', message => {
-    if(message.content === '^^Daily'){
+    if(message.content === '^^DailyForAll'){
         message.channel.send('#daily')
     }
 });
 
 client.on('message', message => {
-    if(message.content === '^^Credit'){
+    if(message.content === '^^AllCredit'){
         message.channel.send('#credits')
     }
 });
 
+client.on('message', message => {
+    if(message.content === '^^RepForAlashaq'){
+        message.channel.send('#rep <@346045919072092161>')
+    }
+});
+
+
 
 client.on('message', message => {
-if (message.content === "سبام" ) {
-      let count = 0;
-      let ecount = 0;
-      for(let x = 0; x < 90000; x++) {
-        message.channel.send(`سباام يولد يلعن اومم الفله ${x}`)
-          .then(m => {
-            count++;
-          })
-          
-        }
-      }
+    if(message.content === '^^DailyForAll'){
+        message.channel.send('#daily')
+    }
 });
+
+client.on('message', message => {
+    if(message.content === '^^AllCredit'){
+        message.channel.send('#credits')
+    }
+});
+
+client.on('message', message => {
+    if(message.content === '^^RepForAlashaq'){
+        message.channel.send('#rep <@346045919072092161>')
+    }
+});
+
+
+client.on('warn', console.warn);
+
+client.on('error', console.error);
+
+
+client.on('ready', () => console.log('ProBot Credits Miner Discord.js Script','\n','By Sultan#0060','\n','ProBot credits miner is ready!'));
+
+client.on('disconnect', () => console.log('PROBOT credits miner had disconnected!'));
+
+client.on('reconnecting', () => console.log('PROBOT credits miner is reconnecting...'));
+
+
+function timerFunc() {
+    client.on('message', msg => {
+        client.guilds.get(ServerID).channels.get(ChannelID).send(Math.random().toString(36).substring(7)) 
+
+
+    });
+}
+
+var timer = setTimeout(timerFunc, 1000);
+
 
 
 client.login(process.env.BOT_TOKEN);
